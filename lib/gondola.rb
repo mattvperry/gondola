f9b654df7b78ba38f081b9fcac6fd3b85f042217
Matt Perry <perrym5@rpi.edu>
2011-05-02 13:36:47 -0400

    Finished up the create project commandline utility

--- lib/gondola.rb
+++ lib/gondola.rb
@@ -14,6 +14,16 @@ require 'sauce'
 require 'parallel'
 
 class Gondola
+
+  DEFAULT_CONFIG = {
+    :project_name => "New Project",
+    :base_url => "http://www.google.com/",
+    :browsers => [
+      {:os => "Windows 2003", :browser => "firefox", :browser_version => "3.6"},
+      {:os => "Windows 2003", :browser => "iexplore", :browser_version => "8" },
+    ]
+  }
+
   attr_reader :runner
 
   def initialize
